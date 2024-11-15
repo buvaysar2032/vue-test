406a6246f049c7259146d9bb3c0ee434b32dfde8
b.kataev <buvaysar2032@gmail.com>
2024-11-15 18:54:38 +0300

    initial commit

---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from "./Card.vue";
+
+const onClickAdd = () => {
+  alert('Добавить');
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+        title="Мужские кроссовки Nike Blazer Mid Suede"
+        imageUrl="/sneakers/sneakers-1.jpg"
+        :price="5000"
+        :isFavorite="false"
+        :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
